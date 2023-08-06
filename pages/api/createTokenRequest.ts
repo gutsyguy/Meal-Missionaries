df615fd8e029a42805382e4f67c1d44258d7ae7d
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-08-06 16:41:45 -0700

    Str

--- pages/api/createTokenRequest.ts
+++
@@ -1,7 +0,0 @@
-import Ably from "ably/promises";
-
-export default async function handler(req:any, res:any) {
-    const client = new Ably.Realtime("WnHnCQ.3eP48A:lxRpyff5eL-5c-n_uMtwOsajWtsCTGL5MTJppGgBzQ4");
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-    res.status(200).json(tokenRequestData);
-};
